113b0c7cc63dcdfc25e434abe810f496b2d9def1
Vale <valentinahdz0296@gmail.com>
2024-10-02 21:38:32 -0600

    Se agrega seccion de tendencias y categorias

--- src/main.js
+++ src/main.js
@@ -3,20 +3,39 @@ async function  getTrendingMoviesPreview() {
     const data = await res.json();
 
     const movies = data.results;
+
     movies.forEach(movie => {
         const trendingPreviewMoviesContainer =document.querySelector('#trendingPreview .trendingPreview-movieList')
 
-        const movieContainer = document.createElement('div');
-        movieContainer.classList.add('movie-container');
-        
-        const movieImg = document.createElement('img');
-        movieImg.classList.add('movie-img');
-        movieImg.setAttribute('alt', movie.title);
-        movieImg.setAttribute('src','https://image.tmdb.org/t/p/w300' + movie.poster_path);
-        movieContainer.appendChild(movieImg);
-        trendingPreviewMoviesContainer.appendChild(movieContainer);
+    const billboard=`
+        <div class="movie-container">
+            <img
+              src="https://image.tmdb.org/t/p/w300/${movie.poster_path}"
+              class="movie-img"
+              alt=${movie.title}/>
+          </div> `
+          trendingPreviewMoviesContainer.innerHTML += billboard
     });
     console.log( {data , movies});
 }
 
+async function  getCategoriesPreview() {
+    const res = await fetch('https://api.themoviedb.org/3/genre/movie/list?api_key=' + API_KEY);
+    const data = await res.json();
+
+    const categories = data.genres;
+        categories.forEach(category => {
+        const previewCategoriesContainer = document.querySelector('#categoriesPreview .categoriesPreview-list')
+
+    const cat=`
+          <div class="category-container">
+            <h3 class="category-title" id ="id${category.id}">
+            ${category.name}
+            </h3>
+          </div>`
+          previewCategoriesContainer.innerHTML += cat
+    });
+}
+
 getTrendingMoviesPreview();
+getCategoriesPreview();
